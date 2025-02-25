10160b770d0f6733fdd6d361ea9474b6d40e9473
prasitchai-789 <prasitchai.f@gmail.com>
2025-02-25 20:10:31 +0700

    graph

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,8 @@ export default defineConfig({
                 'node_modules/glightbox/dist/css/glightbox.min.css',
                 'resources/js/pages/gallery.js',
                 'resources/js/pages/charts-apex.js',
-                'resources/js/pages/graph-palm.js'
+                'resources/js/pages/graph-palm.js',
+                'resources/js/pages/graph-price.js',
             ],
             refresh: true,
         }),
